336ed93851581cfb1474b02ef9e728855d371062
Uly Li <ulysses142@gmail.com>
2024-02-05 01:49:18 -0500

    track create_bills_table

--- migrations/20240202211126_create_bills_table.js
+++ migrations/20240202211126_create_bills_table.js
@@ -3,15 +3,18 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.dropTableIfExists("bills").then(function () {
-    return knex.schema.createTable("bills", function (table) {
-      table.increments("id").primary();
-      table.string("bill_ID"); // Rename to match seed data
-      table.string("title", 1000); // Keep the same name as in seed data
-      table.string("session_ID"); // Rename to match seed data
-      table.date("introduced"); // Rename to match seed data
-      table.timestamp("created_at").defaultTo(knex.fn.now());
-    });
+  return knex.schema.createTable("BillDetails", function (table) {
+    table.increments("id").primary();
+    table.string("bill_ID"); // Rename to match seed data
+    table.string("session_ID"); // Rename to match seed data
+    table.string("title", 1000); // Keep the same name as in seed data
+    table.date("introduced"); // Rename to match seed data
+    table.text("intent"); // JSON content, so use text type
+    table.text("proposed_changes"); // JSON content, so use text type
+    table.text("pros"); // JSON content, so use text type
+    table.text("cons"); // JSON content, so use text type
+    table.text("progress"); // JSON content, so use text type
+    table.timestamp("created_at").defaultTo(knex.fn.now());
   });
 };
 
@@ -20,5 +23,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTable("bills");
+  return knex.schema.dropTable("BillDetails");
 };
